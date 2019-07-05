6640467c479e76e8fe82e489df9cdbc5d4258e62
rechentechnische Projekte der Alten Geschichte Leipzig <ecomp-shONgit@users.noreply.github.com>
2019-07-05 10:45:51 +0200

    Update main.rs

--- dge-crawler/main.rs
+++ dge-crawler/main.rs
@@ -1,8 +1,26 @@
 
-/*second rust WEBSITE to LIST build
+/*
+* 2017/2019 BTL CTS Project, Prof. Charlotte Schubert, Alte Geschichte Leipzig
+* Scope: WEBSITE Proyecto Diccionario Griego-Español to LIST build
 *
-* cargo build lili
-* cargo run
+* Usage
+* 1. build cargo project add main.rs, modifie Cargo.toml and add 
+*    [dependencies]
+*    reqwest = "0.9.16"
+* 2. cargo run
+*
+* GPLv3 copyrigth
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*   
 * 
 */
 
